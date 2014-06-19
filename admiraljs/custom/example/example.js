b416c84ebda417f7ba4368a4874584fecbfe6ba8
StudioDeTelevision <studiodetelevision@gmail.com>
2014-06-19 03:36:49 +0200

    samples

---
+++ admiraljs/custom/example/example.js
@@ -0,0 +1,25 @@
+define(["./editors/editors",'css!./style/style.css'],function(Editors) {
+
+
+var MyCustom=function() {
+	
+	
+	// you might add your custom editors , style etc in this folder
+	// to keep the admin plugin clean
+	
+	new Editors();
+
+	
+
+	
+	
+
+	
+}
+
+
+return MyCustom;
+
+
+	
+});
